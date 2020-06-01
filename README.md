0603225e055dfbeb8ad2f608ce0a1349d5834aae
Juan David Arcila Moreno <jarcil13@eafit.edu.co>
2020-06-01 09:08:05 -0500

    Adding new version of script squeue-apolo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # apolo-user-tools
 Tools to help users.
+
+
+Fields
+### List of available columns in the dataframe
+['account', 'accrue_time', 'admin_comment', 'alloc_node', 'alloc_sid', 'array_job_id', 'array_max_tasks', 
+'array_task_id', 'array_task_str', 'assoc_id', 'batch_features', 'batch_flag', 'batch_host', 'billable_tres',
+'bitflags', 'boards_per_node', 'burst_buffer', 'burst_buffer_state', 'command', 'comment', 'contiguous',
+'core_spec', 'cores_per_socket', 'cpu_freq_gov', 'cpu_freq_max', 'cpu_freq_min', 'cpus_alloc_layout', 
+'cpus_allocated', 'cpus_per_task', 'cpus_per_tres', 'dependency', 'derived_ec', 'eligible_time', 'end_time',
+'exc_nodes', 'exit_code', 'features', 'group_id', 'job_id', 'job_state', 'last_sched_eval', 'licenses',
+'max_cpus', 'max_nodes', 'mem_per_cpu', 'mem_per_node', 'mem_per_tres', 'min_memory_cpu', 'min_memory_node', 
+'name', 'network', 'nice', 'nodes', 'ntasks_per_board', 'ntasks_per_core', 'ntasks_per_core_str', 'ntasks_per_node', 
+'ntasks_per_socket', 'ntasks_per_socket_str', 'num_cpus', 'num_nodes', 'partition', 'pn_min_cpus', 'pn_min_memory',
+'pn_min_tmp_disk', 'power_flags', 'preempt_time', 'priority', 'profile', 'qos', 'reboot', 'req_nodes', 'req_switch',
+'requeue', 'resize_time', 'restart_cnt', 'resv_name', 'run_time', 'run_time_str', 'sched_nodes', 'shared',
+'show_flags', 'sockets_per_board', 'sockets_per_node', 'start_time', 'state_reason', 'std_err', 'std_in',
+'std_out', 'submit_time', 'suspend_time', 'system_comment', 'threads_per_core', 'time_limit', 'time_limit_str',
+'time_min', 'tres_alloc_str', 'tres_bind', 'tres_freq', 'tres_per_job', 'tres_per_node', 'tres_per_socket',
+'tres_per_task', 'tres_req_str', 'user_id', 'wait4switch', 'wckey', 'work_dir', 'wait_time', 'user_name']
+
---
+++ src/slurm/squeue-apolo.py
@@ -0,0 +1,138 @@
+import pyslurm
+import pandas as pd
+from tabulate import tabulate
+
+from datetime import datetime
+import argparse
+import pwd
+
+def get_wait(x):
+  """Get time difference between timestamp and now
+
+  Paramaters
+  ----------
+  str
+      Timestamp with the format of Slurm
+
+  Returns
+  -------
+  deltatime
+     difference time
+  """
+  return datetime.now() - datetime.fromtimestamp(x)
+
+
+def get_username_by_uid(uid):
+  """Get user name by uid of the system
+
+  Paramaters
+  ----------
+  str
+      uid
+
+  Returns
+  -------
+  str
+     user name
+  """
+
+  return pwd.getpwuid(uid).pw_name
+
+
+def get_jobs_info():
+  """Get information of jobs in the queue
+    
+    Returns
+    -------
+    Dataframe
+        Information of the jobs in the queue with wait time and user name
+  """
+
+  df = pd.DataFrame.from_dict(pyslurm.job().get()).T
+  df['wait_time'] = df['submit_time'].apply(get_wait)
+  df['user_name'] = df['user_id'].apply(get_username_by_uid)
+  return df
+
+  
+def get_jobs_waiting_partition(jobs, p=None, reasons=None):
+  """Return the number of jobs waiting in a specific queue
+
+  Parameters
+  ----------
+  df: DataFrame
+    data frame with the information of the jobs
+  p: str
+    name of the partition. If none is specified, all queues are taking into account
+  raesons: list(str)
+    names of the reasons that are valid. If none is specified all reasons are valid.
+    the name of the reason must match with the ones in slurm. 
+    
+
+  Returns
+  -------
+  Dataframe
+     Selected jobs
+  """
+  selected_jobs = pd.DataFrame()
+  # Selected by partition
+  if(p is not None):
+    selected_jobs = jobs[jobs['partition'] == p]
+   
+  # A job that is waiting is a jobs that is not running
+  selected_jobs = selected_jobs[~(selected_jobs['job_state'] == 'RUNNING')]
+  
+  # Selected valid reasons
+  if(reasons is not None and len(reasons) > 0):
+    selected_jobs = selected_jobs[selected_jobs['state_reason'].isin(reasons)]
+  return selected_jobs
+
+def printTabulate(df, headers='keys', tablefmt='psql'):
+  """Wrapper to print df using tabulate
+  """
+  
+  print(tabulate(df), headers=headers, tablefmt=tablefmt)
+
+
+def main():
+  parser = argparse.ArgumentParser(prog='squeue-apolo'
+                                   description='''
+                                               A custom utility to get information 
+                                               about Apolo's queue state''',
+                                   epilog='''
+                                   Only jobs waiting for valid reasons are take into
+                                   account to display information. Valid reasons are:
+                                   Resources, Priority and Reservation; not QOS reasons''')
+  args = parser.parse_args()
+  ## TODO use args to create special behavior
+
+  df = get_jobs_info() 
+  
+  print("Mean wait time all active jobs:")
+  print("%s with +/- %s" % (df['wait_time'].mean(),df['wait_time'].std()))
+  print()
+
+  print("Quartiles:")
+  printTabulate(df['wait_time'].quantile([0.25,0.5,0.85]))
+  print()
+
+  print("Top 10 wait time jobs")
+  printTabulate(df.nlargest(10,'wait_time')[['user_name','wait_time','partition','job_state','state_reason', 'time_limit_str']])
+  print()
+
+  valid_reasons_to_wait = ['Resources', 'Reservation', 'Priority']
+  n_accel_waiting_jobs = len(get_jobs_waiting_partition(df, 'accel', valid_reasons_to_wait))
+  n_accel_2_waiting_jobs =  len(get_jobs_waiting_partition(df, 'accel-2', valid_reasons_to_wait))
+
+  print("Number of Jobs Waiting for GPU")
+  print("Accel:    %i" % n_accel_waiting_jobs)
+  print("Accel-2:  %i" % n_accel_2_waiting_jobs)
+  print("Total:    %i" % (n_accel_waiting_jobs + n_accel_2_waiting_jobs))
+  print()
+  print("Top 5 users by number of active jobs")
+  printTabulate(df.groupby('user_name')['job_id'].agg('count').nlargest(5))
+  print()
+  print("Generated at: %s" % datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+
+  
+if __name__ == "__main__":
+  main()
